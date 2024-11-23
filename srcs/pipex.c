ee89ef3ea9ec09efee29422b9672a133550b4b12
dotacow <dotacowidkmynamesry@gmail.com>
2024-11-24 02:07:47 +0300

    updated checklist

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,20 +6,25 @@
 /*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 00:55:32 by yokitane          #+#    #+#             */
-/*   Updated: 2024/11/24 02:02:00 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/11/24 02:07:32 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+//check infile and outfile
+	//if outfile doesnt exist, create it
+	//if infile doesnt exist, print error
 //pipe
 //fork
 //child process
-//dup2
-//execve
+	//dup2
+	//execve
+		//get path from env(?)
 //parent process
-//waitpid
-//dup2
-//execve
+	//waitpid
+	//dup2
+	//execve
+		//get path from env(?)
 //exit
 int		main(int argc, char **argv);
