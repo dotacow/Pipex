9e6ffe87b36f11674fc45e3e4b5c37dc195d01d3
Yousef Kitaneh <yokitane@c1r7s2.42amman.com>
2024-11-24 18:46:47 +0300

    please god make it stop

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -3,28 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 00:55:32 by yokitane          #+#    #+#             */
-/*   Updated: 2024/11/24 02:07:32 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/11/24 18:46:40 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-//check infile and outfile
-	//if outfile doesnt exist, create it
-	//if infile doesnt exist, print error
-//pipe
-//fork
-//child process
-	//dup2
-	//execve
-		//get path from env(?)
-//parent process
-	//waitpid
-	//dup2
-	//execve
-		//get path from env(?)
-//exit
-int		main(int argc, char **argv);
+void execute(char *cmd, char **env)
+{
+	char	**paths;
+	int		i;
+
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	pid_t	id1;
+	pid_t	id2;
+	int		pipe_fd[2];
+
+	if (argc == 5)
+	{
+	if (access(argv[1], R_OK | F_OK) == -1)
+		perror("infile error, are you sure it exists?");
+	if (pipe(pipe_fd) == -1)
+		perror("pipe faliure"); // replace with exit handler
+	if ((id1 = fork()) == -1)
+		perror("fork faliure");//add closing  replace with exit handler
+	if (id1 == 0)
+		child_p1(argv, envp, pipe_fd, &id1);// build later
+	else
+	{
+		if ((id2 = fork()) == -1)
+			perror("fork faliure");//add closing  replace with exit handler
+		if (id2 == 0)
+			child_p2(argv, envp, pipe_fd, &id2);//build later
+	}
+	wait(NULL);
+	wait(NULL);
+	return (0);
+	}
+}
+
--- srcs/utils.c
+++ srcs/utils.c
@@ -1,22 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/24 00:30:31 by yokitane          #+#    #+#             */
-/*   Updated: 2024/11/24 15:19:28 by yokitane         ###   ########.fr       */
+/*   Created: 2024/11/24 14:48:16 by yokitane          #+#    #+#             */
+/*   Updated: 2024/11/24 15:03:46 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../includes/pipex.h"
 
-# include "../libft/libft.h"
-# include <sys/wait.h>
-# include <errno.h>
-# include <stdio.h>
+char **get_path(char **envp)
+{
+	int		i;
+	char	**paths;
 
-char **get_path(char **envp);
-#endif
+	i = 0;
+	paths = NULL;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			paths = ft_split(envp[i] + 5, ':');
+			break;
+		}
+		i++;
+	}
+	if (paths == NULL)
+	{
+		perror("Error: PATH not found in environment variables");
+		exit(EXIT_FAILURE);
+	}
+	return (paths);
+}
