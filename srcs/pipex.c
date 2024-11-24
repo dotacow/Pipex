dc4fb9d057775a55353d7fdfaa467afacd15d85d
Yousef Kitaneh <yokitane@c2r4s15.42amman.com>
2024-11-24 19:14:16 +0300

    slight tweaks

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 00:55:32 by yokitane          #+#    #+#             */
-/*   Updated: 2024/11/24 18:46:40 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:14:10 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,29 @@ void execute(char *cmd, char **env)
 
 int		main(int argc, char **argv, char **envp)
 {
-	pid_t	id1;
-	pid_t	id2;
-	int		pipe_fd[2];
+	t_data	data;
 
-	if (argc == 5)
-	{
-	if (access(argv[1], R_OK | F_OK) == -1)
+	if (argc != 5)
+		perror("Invalid usage: ./pipex infile cmd1 cmd2 outfile");
+	if (access(argv[1], R_OK) == -1)
 		perror("infile error, are you sure it exists?");
-	if (pipe(pipe_fd) == -1)
+	if (pipe(data.pipe_fd) == -1)
 		perror("pipe faliure"); // replace with exit handler
-	if ((id1 = fork()) == -1)
+	data.id1 = fork();
+	if (data.id1 == -1)
 		perror("fork faliure");//add closing  replace with exit handler
-	if (id1 == 0)
-		child_p1(argv, envp, pipe_fd, &id1);// build later
-	else
+	if (data.id1 == 0)
+		child_p1(argv,envp, data);// build later
+	else // parent
 	{
-		if ((id2 = fork()) == -1)
+		data.id2 = fork();
+		if (data.id2 == -1)
 			perror("fork faliure");//add closing  replace with exit handler
-		if (id2 == 0)
-			child_p2(argv, envp, pipe_fd, &id2);//build later
+		if (data.id2 == 0)
+			child_p2(argv, envp, data);//build later
 	}
-	wait(NULL);
-	wait(NULL);
+	wait(NULL); // calling these okay in child
+	wait(NULL); // ..........................^
 	return (0);
-	}
 }
 
